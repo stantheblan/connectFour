ce058e981b2a59097dd350966d62f35962681326
Stan <stantonmoore42@gmail.com>
2022-02-17 23:16:00 -0500

    working win condition

--- game.js
+++ game.js
@@ -18,6 +18,7 @@ function makeHtmlBoard()
     {
         const headCell = document.createElement('td');
         headCell.setAttribute('id', x);
+        headCell.setAttribute('class', 'test');
         topR.append(headCell);
     }
     board.append(topR);
@@ -30,16 +31,26 @@ function makeHtmlBoard()
         {
             const cell = document.createElement('td');
             cell.setAttribute('id', `${y}-${x}`);
+            cell.setAttribute('class', `test`);
             row.append(cell);
         }
         board.append(row);
     }
+
+    // //make reset button
+    // const rButton = document.createElement("button");
+    // rButton.setAttribute('class', 'resett');
+    // let t = document.createTextNode("Resettttt");
+    // rButton.appendChild(t);
+    
+    // board.append(rButton)
 }
 
 function findCellforCol(x) 
 {
     for (let y = tableHeight - 1; y >= 0; y--) 
     {
+        //if not empty then drop in
         if (!board[y][x]) 
         {
             return y;
@@ -48,18 +59,26 @@ function findCellforCol(x)
     return null;
 }
 
-function placeInTable(y, x) 
+function placeInTable(x, y) 
 {
     let piece = document.createElement('div');
     piece.classList.add('piece');
-    piece.classList.add(`p${currPlayer}`);
+    piece.classList.add(`p${cPlayer}`);
     piece.style.top = -50 * (y + 2);
 
     let cell = document.getElementById(`${y}-${x}`);
     cell.append(piece);
 }
 
-function clickHandler(e) {
+function boardReset()
+{
+    board.forEach(e => {
+        e = null;
+    });
+}
+
+function clickHandler(e) 
+{
     //get x from id of clicked cell
     const x = e.target.id;
 
@@ -68,17 +87,76 @@ function clickHandler(e) {
     if (y === null) {return;}
 
     //place piece in board and add to HTML table
-    board[y][x] = currPlayer;
-    placeInTable(y, x);
+    board[y][x] = cPlayer;
+    placeInTable(x, y);
+    
+    // check win
+    if (checkForWin()) 
+    {
+        return eGame(cPlayer);
+    }
+    
+    // check tie
+    if (board.every(row => row.every(cell => cell))) 
+    {
+        return eGame('Tie!');
+    }
+    //switch player
+    cPlayer = cPlayer === 1 ? 2 : 1;
+}
 
-    //switch
-    currPlayer = currPlayer === 1 ? 2 : 1;
+function checkForWin() {
+    for (let y = 0; y < tableHeight; y++) {
+        for (let x = 0; x < tableWidth; x++) {
+            //4 cells for each of the different directions starting at 0, 0
+            const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+            const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+            const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+            const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+    
+            //find winner 
+            if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) 
+            {
+                return true;
+            }
+        }
+    }
+}
+
+function _win(cells) {
+    //check four cells to see if they're all color of current player
+    //cells: list of four (y, x) cells
+    //returns true if all coordinates are on board & all equal cPlayer
+      return cells.every(
+          ([y, x]) =>
+              y >= 0 &&
+              y < tableHeight &&
+              x >= 0 &&
+              x < tableWidth &&
+              board[y][x] === cPlayer
+          );
+  }
+
+function eGame(player)
+{
+    alert("Player " + player + " wins;")
+    console.log("Player " + player + " wins;")
+    console.log(board)
+    boardReset()
+    console.log(board)
+
+    //trying to get every cells bg to be grey after win
+    // const arr = document.querySelectorAll(".test");
+    // console.log(arr)
+    // arr.forEach(e => {
+    //     e.style.color = "grey"
+    // });
 }
 
 const tableWidth = 7;
 const tableHeight = 6;
 
-let currPlayer = 1;
+let cPlayer = 1;
 let board = []; 
 
 makeBoard();
